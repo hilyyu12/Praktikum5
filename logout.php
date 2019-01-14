edbcce1c6445e32bbd478c1f2c6dd5b84c623da0
abiyyu hilmi <abiyyumi1112@gmail.com>
2019-01-14 12:35:15 +0700

    first commit

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+    if(isset($_SESSION["user"]))
+    {
+        header("location:index.php");
+    }
+
+session_destroy();
+    header("location:index.php");
+
+
+
+
+?>
